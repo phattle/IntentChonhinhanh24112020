3067a110d804c33e826289ca236372c8cb520228
phatdroid94 <phatdroid94@gmail.com>
2021-01-21 19:08:33 +0700

    xử lý hình real

--- app/src/main/java/com/example/intentchonhinhanh24112020/PlayGameActivity.java
+++ app/src/main/java/com/example/intentchonhinhanh24112020/PlayGameActivity.java
@@ -4,25 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class PlayGameActivity extends AppCompatActivity {
 
     TextView mTvScore,mTvTime;
     ImageView mImgReal,mImgPick;
     MyCountDown mMyCountDown;
+    String[] mArrAnimals;
+    int mResourceImgRandom = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
-
         init();
+        mapView();
         event();
+    }
 
+    private void mapView() {
+        int index = new Random().nextInt(mArrAnimals.length);
+        mResourceImgRandom = getResources().getIdentifier(mArrAnimals[index],"drawable",getPackageName());
+        mImgReal.setImageResource(mResourceImgRandom);
     }
 
     private void event() {
@@ -36,7 +46,6 @@ public class PlayGameActivity extends AppCompatActivity {
                 mTvTime.setText("Time : "  + timeCurrent / 1000);
             }
         });
-
 
         // Bắt sự kiện chuyển qua màn hình chọn danh sách hình
         mImgPick.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +62,9 @@ public class PlayGameActivity extends AppCompatActivity {
         mTvTime = findViewById(R.id.textViewTime);
         mImgReal = findViewById(R.id.imageViewImageReal);
         mImgPick = findViewById(R.id.imageViewImagePick);
+
+        // Truyền giá trị từ file string array
+        mArrAnimals = getResources().getStringArray(R.array.arrAnimals);
 
         mMyCountDown = MyCountDown.getInstance();
     }
